034a49180cea8e8ea39734a44e0f30c4f8030ff8
Cedric Johnson <cjohns0913@gmail.com>
2016-06-29 16:35:15 -0400

    added Java interface UserDao; kind of forgot :grin:

--- java-server/src/main/java/com/cooksys/ftd/celebrity/news/dao/UserDao.java
+++ java-server/src/main/java/com/cooksys/ftd/celebrity/news/dao/UserDao.java
@@ -1,5 +1,16 @@
 package com.cooksys.ftd.celebrity.news.dao;
 
-public class UserDao {
+import java.util.Optional;
 
+import com.cooksys.ftd.celebrity.news.model.User;
+
+public class UserDao extends AbstractDao {
+	
+	public User createUser(User user) {
+		return null; // TODO
+	}
+	
+	public Optional<User> getUserByUsername(String username) {
+		return null; // TODO
+	}
 }
